49a79c9ca48b27f81f1575609a2ef881c7ee3609
_Iskra_ <_Iskra_@DESKTOP-4SUHQ4F>
2024-08-01 21:09:34 +0300

    #Task1 Сделано и настроено задание

---
+++ src/main/java/com/example/testpostgre/service/truckrepairservice/TruckRepairService.java
@@ -0,0 +1,26 @@
+package com.example.testpostgre.service.truckrepairservice;
+
+import com.example.testpostgre.dto.TruckRepairDTO;
+import com.example.testpostgre.model.TruckRepair;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TruckRepairService {
+    public List<TruckRepairDTO> findWithRemainingRepairDays();
+
+    void save(TruckRepair timetable);
+
+    int[] saveStudentsList(List<TruckRepair> timetables);
+
+    void update(TruckRepair timetable);
+
+    void delete(TruckRepair timetable);
+
+    List<TruckRepair> findAll();
+
+    void deleteAll();
+
+
+    Optional<TruckRepair> findById(Long id);
+}
